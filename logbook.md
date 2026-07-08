3a4e16627b296ba4df40d7595ccb1ff61539a2ab
annaasofia <annasofia.moro@studenti.unipd.it>
2026-07-08 18:56:47 +0200

    day6

--- logbook.md
+++ logbook.md
@@ -1,5 +1,10 @@
 # Traineeship al CERN
 
+1. [week 1](#week-1)
+2. [week 2](#week-2)  
+    - [measuring the channeling efficiency](#measure-the-channeling-efficiency)
+3. [week 3](#week-3)
+
 ## WEEK 1  
 
 books/articles to read: 
@@ -54,12 +59,21 @@ what to do next:
 - look at the angle values from the [articles](./aboutcrystals/s10052-025-15092-y.pdf)  
     | | | TCCP | TCCPA |
     |-|-|-----|-------|
+    |length|[mm]|70|70.5|
+    |width|[mm]|8|22.5|
+    |height|[mm]|2|2|
     |bend radius $\rho$ | [m]|10|5.3|
     |bend angle $\theta_b$ | [mrad]|7.0|13.3|
     |$\theta_L$ @ 180 GeV/c | [$\mu$ rad]|12.9|12.5|  
 $\theta_L = (1-\frac{\rho_c}{\rho})\sqrt{\frac{2U_0}{E}}$ where: $\rho_c=\frac{E}{U'(x_c)}$, $U'(x_c)=5.7$ GeV/cm, $U_0=16$ eV for Silicon (110) 
-- measure the channeling efficiency $\epsilon_{ch}=\frac{N_{ch}}{N_{tot}}\cdot 100$, by doing a gaussian fit only from the right side of the peak, where $N_{ch}$ are identified from $\Delta\theta_x$ and $N_{tot}$ are considered from the ones that entered the crystal within the angle $\pm\frac{1}{2}\theta_L$, so those with the potential to be channeled.  
-- _considering the torsion_ $\tau_y$ _correction:_ the effect of torsion (the variation of the crystalline plane orientation along the direction ($y$) perpendicular to the bending plane ($x$)) must be accounted for when measuring the channelling efficiency. The different orientations of the crystallographic planes along the crystal front surface influence the angular range in which particles can be channelled, i.e., the cut at $\pm 1/2 \,\theta_L$ may not be centred on zero, but a correction parameter $\theta_0 = \theta_0 (y)$. So when filtering the data, instead of choosing particles with $|\theta_{in,x}|\leq\frac{1}{2}\theta_L$, we choose them $|\theta_{in,x}-\theta_0|\leq\frac{1}{2}\theta_L$.
-    - in [checkdata_2.py](./recoDataSimple/checkdata_2.py) we treat the crystal as if it were ideal, with negligible torsion, and find a single global $\theta_{optimal}$ from the peak of the angular scan.
-    - what the articles does is that it takes the beam “spot” (`d0_x` vs `d0_y`) and divides it into a checkerboard grid. For each square on the grid, it performs an angular scan (like my previous h_scan) and finds the specific peak angle $\theta_0$ for that square. It then reapplies a correction to each particle by subtracting the local $\theta_0$.
+
+### Measure the channeling efficiency $\epsilon_{ch}=\frac{N_{ch}}{N_{tot}}\cdot 100$
+
+- center the angle and choosing $\theta_0$ for torsion correction $\tau_y$, which is the variation of the crystalline plane orientation along the direction ($y$) perpendicular to the bending plane ($x$), and must be accounted for (the different orientations of the crystallographic planes along the crystal front surface influence the angular range in which particles can be channelled, i.e., the cut at $\pm 1/2 \,\theta_L$ may not be centred on zero, but a correction parameter $\theta_0 = \theta_0 (y)$)
+    - in [checkdata_2.py](./recoDataSimple/checkdata_2.py) we treat the crystal as if it was ideal, and we find a single global $\theta_{optimal}$ from the peak of the angular scan
+    - what the articles does is that it takes the beam “spot” (`d0_x` vs `d0_y`) and divides it into a checkerboard grid. For each square on the grid, it performs an angular scan (like my previous h_scan) and finds the specific peak angle $\theta_0$ for that square. It then reapplies a correction to each particle by subtracting the local $\theta_0$ $\to$ [checkdata_3.py](./recoDataSimple/checkdata_3.py)
+- $N_{tot}:$ filter the data set, choosing the events that produced a single track, have a good $\chi^2$ value (?), have actually entered the crystal, and have an angle within $\pm\frac{1}{2}\theta_L$ (actually we choose them $|\theta_{in,x}-\theta_0|\leq\frac{1}{2}\theta_L$):
+    - to filter spatially those who entered the crystal, look at `d0` variables, and considering the width and the height of the crystals
+- identify $N_{ch}$: on the $\Delta\theta_x$ filtered histogram perform a gaussian fit on the channeling peak, only from the right side of the peak, then do the integral around the peak
+- compute $\epsilon_{ch}=\frac{N_{ch}}{N_{tot}}\cdot 100$ amd its error - we choose a binomial distribution
 
--- recoDataSimple/checkdata_2.py
+++ recoDataSimple/checkdata_2.py
@@ -8,20 +8,26 @@ ROOT.ROOT.EnableImplicitMT()
 file = 8430
 filename = "recoDataSimple_" + str(file) + "_xtalMerging.root"
 
-ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptStat(1111)
 ROOT.gStyle.SetPalette(ROOT.kBird)
 
 # VARIABLES
 theta_L = 0 # Lindhard angle for TCCP and TCCPA in urad
+width = 0 # width of the crystal in mm (for spatial cut)
+height = 0 # height of the crystal in mm (for spatial cut)
 theta_0 = 0 # centro della tua isola di channeling - looking at 2D "h_scan" find on which x (thetaIn) the channeling isle is centered
 max_value = 0 # histograms range
 deflection_peak = 6010.0 # urad
 
 if file in [8430, 8431, 8650]:
     theta_L = 12.9
+    width = 8
+    height = 2
     max_value = 8000
 elif file in [8655, 8656]: 
     theta_L = 12.5
+    width = 22.5
+    height = 2
     max_value = 14000
 else:
     print("Run not found...")
@@ -32,11 +38,33 @@ df = ROOT.RDataFrame("simpleEvent", filename)
 print("="*50)
 print(f"Analyzing {filename} ...")
 
-# FILTERING the data and conversion from rad to urad
-df_clean = df.Filter("SingleTrack == 1")
+# FILTERING the data (single tracks and have entered the crystal) and conversion from rad to urad
+# FILTER 1: SingleTrack == 1
+df_phys = df.Filter("SingleTrack == 1")
+P0 = df.Count().GetValue()
+P1 = df_phys.Count().GetValue()
+df_phys = df_phys.Define("thetaIn_x", "Tracks.thetaIn_x * 1e6").Define("Deltatheta_x", "(Tracks.thetaOut_x - Tracks.thetaIn_x) * 1e6")
 print("="*50)
-print(f"{((df.Count().GetValue() - df_clean.Count().GetValue())/df.Count().GetValue()*100):.2f}% events out of {df.Count().GetValue()} got discarded (SingleTrack = 0).")
-df_phys = df_clean.Define("thetaIn_x", "Tracks.thetaIn_x * 1e6").Define("Deltatheta_x", "(Tracks.thetaOut_x - Tracks.thetaIn_x) * 1e6")
+print(f"Filter 1 (SingleTrack == 1): {((P0 - P1)/P0*100):.2f}% events out of {P0} got discarded.")
+
+# FILTER 2: good chi squared (chi2 < value)
+good_chi2_value = 5
+df_phys = df_phys.Filter(f"Tracks.chi2_x < {good_chi2_value} && Tracks.chi2_y < {good_chi2_value}")
+P2 = df_phys.Count().GetValue()
+print(f"Filter 2 (chi2 < {good_chi2_value}): {((P1 - P2)/P1*100):.2f}% events out of {P1} got discarded.")
+
+# FILTER 3: spatial cut (d0_x and d0_y within the crystal area)
+# i look at the beam profile of d0 of those who channeled, and find the region width x length
+# ---------------------------------------------------------------------------------------------
+# d0_x = df_phys.Histo1D(("d0_x", "d0_x; d0_x [mm]; Counts", 100, -20, 30), "Tracks.d0_x")
+# d0_y = df_phys.Histo1D(("d0_y", "d0_y; d0_y [mm]; Counts", 100, -20, 30), "Tracks.d0_y")
+# x_c = d0_x.GetMean() # centro del cristallo in x (mm)
+# y_c = d0_y.GetMean() # centro del cristallo in y (mm)
+# x_min, x_max = x_c - width / 2.0, x_c + width / 2.0
+# y_min, y_max = y_c - height / 2.0, y_c + height / 2.0
+# spatial_cut = f"Tracks.d0_x > {x_min} && Tracks.d0_x < {x_max} && Tracks.d0_y > {y_min} && Tracks.d0_y < {y_max}"
+# df_phys = df_phys.Filter(spatial_cut, "Spatial Cut (Crystal Area)")
+# print(f"{((P0 - P2)/P0*100):.2f}% events out of {P0} got discarded (SingleTrack = 0, Spatial Cut).")
 
 # HISTOGRAMS PRE CUT
 h_theta_before = df_phys.Histo1D(("h_theta_before", "#theta_{in,x}; #theta_{x} [#murad]; Counts", 500, -150, 150), "thetaIn_x")
@@ -44,10 +72,16 @@ h_defl_before = df_phys.Histo1D(("h_defl_before", "Angular Deflection; #Delta#th
 h_scan = df_phys.Histo2D(("h_scan", "Angular deflection of the particles as a function of the incident angle; Incident angle #theta_{In, x} [#murad]; Deflection #Delta#theta_{x} [#murad]", 500, -150, 150, 500, -2000, max_value), "thetaIn_x", "Deltatheta_x")
 
 # FINDING THETA 0
-df_theta0a = df_phys.Filter("Deltatheta_x > 5000")
+df_theta0a = df_phys.Filter("Deltatheta_x > 5500")
 h_theta0a = df_theta0a.Histo1D(("h_theta0a", "#theta_{in,x}; #theta_{x} [#murad]; Counts", 500, -150, 150), "thetaIn_x")
 df_theta0b = df_phys.Filter("Deltatheta_x < 5000")
 h_theta0b = df_theta0b.Histo1D(("h_theta0b", "#theta_{in,x}; #theta_{x} [#murad]; Counts", 500, -150, 150), "thetaIn_x")
+
+# SPATIAL CUT
+h_d0_x_ch = df_theta0a.Histo1D(("h_d0_x_ch", "d0_x of channeled particles; d0_x [mm]; Counts", 100, -20, 20), "Tracks.d0_x")
+h_d0_y_ch = df_theta0a.Histo1D(("h_d0_y_ch", "d0_y of channeled particles; d0_y [mm]; Counts", 100, -20, 20), "Tracks.d0_y")
+h_d0_xy_ch = df_theta0a.Histo2D(("h_d0_xy_ch", "d0_x vs d0_y of channeled particles; d0_x [mm]; d0_y [mm]", 200, -5, 5, 200, -10, 10), "Tracks.d0_x", "Tracks.d0_y")
+h_d0_xy = df_phys.Histo2D(("h_d0_xy", "d0_x vs d0_y of all particles; d0_x [mm]; d0_y [mm]", 200, -5, 5, 200, -10, 10), "Tracks.d0_x", "Tracks.d0_y")
 
 # LINDHARD CUT
 # only keeping events whose theta_in is within +/- 0.5 * theta_L
@@ -57,7 +91,7 @@ df_cut = df_phys.Filter(cut)
     
 # how many particles survive the cut (N_tot)
 N_tot = df_cut.Count().GetValue()
-print(f"{(N_tot/df_clean.Count().GetValue()*100):.2f}% events selected after the +/- 1/2 theta_L cut ({(N_tot/df.Count().GetValue()*100):.2f}% of the total).")
+print(f"{(N_tot/df_phys.Count().GetValue()*100):.2f}% events selected after the +/- 1/2 theta_L cut ({(N_tot/df.Count().GetValue()*100):.2f}% of the total).")
 print("="*50)
 
 h_theta_cut = df_cut.Histo1D(("h_theta_cut", "#theta_{in,x} after filtering ; #theta_{x} [#murad]; Counts", 500, -150, 150), "thetaIn_x")
@@ -82,13 +116,20 @@ if N_tot > 0:
     
     # R = usa il range specificato, Q = modalità silenziosa, 0 = non disegnare subito
     h_cut_value.Fit(gaus_fit, "RQ0")
+
+    fit_mean = gaus_fit.GetParameter(1)
+    fit_sigma = gaus_fit.GetParameter(2)
     
     # COMPUTE N CH FROM GAUSSIAN INTEGRAL
     bin_width = h_cut_value.GetBinWidth(1)
-    N_ch = gaus_fit.Integral(fit_min, fit_max) / bin_width
-    
-    fit_mean = gaus_fit.GetParameter(1)
-    fit_sigma = gaus_fit.GetParameter(2)
+    # method 1: integral of the gaussian fit
+    # N_ch = gaus_fit.Integral(5950, max_value) / bin_width
+    # method 2: integral of the histogram
+    bin_min = h_cut_value.FindBin(fit_mean - 3.0 * fit_sigma)
+    # bin_max = h_cut_value.FindBin(fit_mean + 4.0 * fit_sigma)
+    bin_max = h_cut_value.FindBin(max_value)
+    N_ch = h_cut_value.Integral(bin_min, bin_max)
+    print(f"Number of channeled particles (N_ch) = {N_ch:.0f} (from bin {bin_min} to {bin_max})")
     
     # EFFICIENCY WITH ITS ERROR (binomial distribution)
     eff_ch = (N_ch / N_tot) * 100.0
@@ -108,6 +149,8 @@ h_theta_cut.SetLineColor(ROOT.kBlack)
 h_theta0b.Draw("SAME"); h_theta0b.SetLineColor(ROOT.kGreen)
 h_theta0a.Draw("SAME"); h_theta0a.SetLineColor(ROOT.kBlue)
 h_theta_cut.Draw("SAME"); h_theta_cut.SetLineColor(ROOT.kRed)
+
+c0.Update()
 
 c1 = ROOT.TCanvas("c1", "Channeling Efficiency Analysis", 1800, 700)
 c1.Divide(3, 1) 
@@ -138,6 +181,10 @@ h_cut_value.SetLineColor(ROOT.kGreen+2)
 h_cut_value.Draw("HIST")
 if N_tot > 0:
     gaus_fit.Draw("SAME")
+line3 = ROOT.TLine(fit_mean - 3*fit_sigma, 0, fit_mean - 3*fit_sigma, h_cut_value.GetMaximum())
+line3.SetLineColor(ROOT.kRed); line3.SetLineStyle(2); line3.SetLineWidth(2); line3.Draw()
+line4 = ROOT.TLine(fit_mean + 4*fit_sigma, 0, fit_mean + 4*fit_sigma, h_cut_value.GetMaximum())
+line4.SetLineColor(ROOT.kRed); line4.SetLineStyle(2); line4.SetLineWidth(2); line4.Draw()
 
 legend = ROOT.TPaveText(0.45, 0.75, 0.90, 0.90, "NDC")
 legend.SetBorderSize(1)
@@ -151,3 +198,14 @@ legend.Draw()
 print(f"#sigma = {fit_sigma:.2f}, so bin width should be = {3.49*fit_sigma/math.pow(N_tot,1/3)}")
 
 c1.Update()
+
+c2 = ROOT.TCanvas("c2", "d0_x and d0_y of channeled particles", 1800, 900)
+c2.Divide(2, 2)
+c2.cd(1)
+h_d0_x_ch.Draw()
+c2.cd(2)
+h_d0_y_ch.Draw()    
+c2.cd(3)
+h_d0_xy_ch.Draw("COLZ")
+c2.cd(4)   
+h_d0_xy.Draw("COLZ")
